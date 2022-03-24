bb07e877e7df6f891f74fa789e6711f74131fd24
Daniel Huluka <35296291+danhul@users.noreply.github.com>
2022-03-24 13:54:01 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,8 @@ fi
 
 if [ -z "$APP_JAR" ]; then
 	echo "No app name specified, looking for .jar file under build/libs"
+	echo $(pwd)
+	echo ${{ github.action_path }}
 	app=$(find ./build/libs  -type f -name "*.jar")
 	if [ -z "$app" ];
 	then
@@ -28,7 +30,7 @@ if [ -z "$APP_JAR" ]; then
         	else
 			PARAMS="${PARAMS} -F file=@${app}"
 			echo "Deploying:  $app"
-			curl ${PARAMS} ${XP_URL}/app/install
+			echo "going to run curl ${PARAMS} ${XP_URL}/app/install"
 	        fi
 	fi
 	
@@ -37,7 +39,7 @@ else
 	app="$(find . -wholename ${APP_JAR} | head -n 1)"
 	if [ "$app" != "" ]; then
 		PARAMS="${PARAMS} -F file=@${app}"
-		curl ${PARAMS} ${XP_URL}/app/install
+		echo "going to run. curl ${PARAMS} ${XP_URL}/app/install"
 	else
 		echo "Unable to find the specified app/PATH."
 	fi
