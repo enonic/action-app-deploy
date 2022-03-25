5630b305d50ee6619f43c2ff9022aedf8dfab723
Daniel Huluka <35296291+danhul@users.noreply.github.com>
2022-03-25 12:31:05 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -e
 PARAMS="-X POST -f -s -S -o - -u ${XP_USERNAME}:${XP_PASSWORD}" # Initializing arguments for curl command. 
 
 if [ "$CLIENT_KEY" != "" ] && [ "$CLIENT_CERT" != "" ]; then    #Add the MTLS client key and cert in the argumetns list for the curl command.
-    echo "Using mTLS"
+    echo "Using mTLS to establish secure connection with the server"
     KEY=$(mktemp /tmp/key.XXXXXX)
     CERT=$(mktemp /tmp/crt.XXXXXX)
     echo -n "${CLIENT_KEY}" > ${KEY}
@@ -13,37 +13,21 @@ if [ "$CLIENT_KEY" != "" ] && [ "$CLIENT_CERT" != "" ]; then    #Add the MTLS cl
     PARAMS="${PARAMS} --key ${KEY} --cert ${CERT}"
 fi
 
+app=`find . -wholename ${APP_JAR:-"./build/libs/*.jar"}` # Use APP_JAR if set or default to ./build/libs/*.jar
+num_files=`echo "$app" | wc -w`
 
-if [ -z "$APP_JAR" ]; then
-	echo "No app name specified, looking for .jar file under build/libs"
-	#ls -lah ./
-	#ls -lah .git
-	#echo $(pwd)
-	#echo "${{ github.action_path }}"
-	#ls -lah ${{ github.action_path }}
-	app=$(find ./build/libs  -type f -name "*.jar")
-	if [ -z "$app" ];
-	then
-		echo "There is no jar file, under build/libs"
-	else #If it has found one or more .jar files under build/libs
-		num_files=$(echo "$app" | wc -w)
-        	if (( $num_files > 1 ));
-        	then
-                	echo "More than one jar file under build/libs, please specify which one to deploy in action input." 
-        	else
-			PARAMS="${PARAMS} -F file=@${app}"
-			echo "Deploying:  $app"
-			curl ${PARAMS} ${XP_URL}/app/install
-	        fi
-	fi
-	
+if (( $num_files != 1 ));
+then
+    # Failed to find just 1 file, print error and exit
+    echo "This action needs exactly 1 jar but it found $num_files!"
+    for a in $app; do  #Display the .jar files found
+        echo "  $a"
+    done
+    echo "Set 'app_jar' parameter so 1 and only 1 jar is selected!"
+    exit 1
 else
-	echo "App name and path specified"
-	app="$(find . -wholename ${APP_JAR} | head -n 1)"
-	if [ "$app" != "" ]; then
-		PARAMS="${PARAMS} -F file=@${app}"
-		curl ${PARAMS} ${XP_URL}/app/install
-	else
-		echo "Unable to find the specified app/PATH."
-	fi
+        # We have a single jar, set it up for curl
+        PARAMS="${PARAMS} -F file=@${app}"
 fi
+
+curl ${PARAMS} ${XP_URL}/app/install        #run curl with the parameters set above 
