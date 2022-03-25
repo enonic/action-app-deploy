ede5ad6af4002b17cb6cc0a497c8946b6afb0c87
Daniel Huluka <35296291+danhul@users.noreply.github.com>
2022-03-25 08:48:12 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,26 +2,17 @@
 
 set -e
 
-XP_USERNAME=me
-XP_PASSWORD=you
-APP_JAR=$1
-
-
-#echo ${{ github.action_path }}
-
 PARAMS="-X POST -f -s -S -o - -u ${XP_USERNAME}:${XP_PASSWORD}" # Initializing arguments for curl command. 
 
-#if [ "$CLIENT_KEY" != "" ] && [ "$CLIENT_CERT" != "" ]; then    #Add the MTLS client key and cert in the argumetns list for the curl command.
-#    echo "Using mTLS"
-#    KEY=$(mktemp /tmp/key.XXXXXX)
-#    CERT=$(mktemp /tmp/crt.XXXXXX)
-#    echo -n "${CLIENT_KEY}" > ${KEY}
-#    echo -n "${CLIENT_CERT}" > ${CERT}
-#    PARAMS="${PARAMS} --key ${KEY} --cert ${CERT}"
-#fi
+if [ "$CLIENT_KEY" != "" ] && [ "$CLIENT_CERT" != "" ]; then    #Add the MTLS client key and cert in the argumetns list for the curl command.
+    echo "Using mTLS to establish secure connection with the server"
+    KEY=$(mktemp /tmp/key.XXXXXX)
+    CERT=$(mktemp /tmp/crt.XXXXXX)
+    echo -n "${CLIENT_KEY}" > ${KEY}
+    echo -n "${CLIENT_CERT}" > ${CERT}
+    PARAMS="${PARAMS} --key ${KEY} --cert ${CERT}"
+fi
 
-
-#echo "APP_JAR is $APP_JAR"
 app=`find . -wholename ${APP_JAR:-"./build/libs/*.jar"}` # Use APP_JAR if set or default to ./build/libs/*.jar
 num_files=`echo "$app" | wc -w`
 
@@ -32,19 +23,11 @@ then
     for a in $app; do
         echo "  $a"
     done
-    
     echo "Set 'app_jar' parameter so 1 and only 1 jar is selected!"
     exit 1
 else
-    if [[ $file =~ \.jar$ ]];
-    then
         # We have a single jar, set it up for curl
         PARAMS="${PARAMS} -F file=@${app}"
-    else
-        echo "The given input is not a .jar file"
-        exit 1
-    fi
-        
 fi
 
-echo " I am going to run curl ${PARAMS} ${XP_URL}/app/install"
+curl ${PARAMS} ${XP_URL}/app/install
